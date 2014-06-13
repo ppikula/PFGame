56d32cffcac991b04209d1b7f56e753ca2a7c32c
Pawel Pikula <ppikula@gmail.com>
2014-06-13 23:11:58 +0200

    display empty window

--- PF.hs
+++ PF.hs
@@ -1,7 +1,17 @@
 import System.Environment
- 
--- | 'main' runs the main program
-main :: IO ()
-main = getArgs >>= print . haqify . head
- 
-haqify s = "Haq! " ++ s
+
+import Graphics.UI.SDL as SDL
+import Foreign.C
+
+main = do
+    SDL.init initFlagVideo
+    window <- create_window
+    renderer <- getRenderer window
+    --renderFillRect renderer (Ptr (Rect 0 0 800 600) )
+    renderPresent renderer
+    delay 5000
+
+create_window = do
+    title <- newCString "Test Window"
+    SDL.createWindow title SDL.windowPosCentered SDL.windowPosCentered 800 600 0
+
